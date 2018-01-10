baa3332c988b3efa13cc2e04a0beb9fa3ae274fb
Nicholas Vanderpool <nvanderp@MacBook-Pro.attlocal.net>
2018-01-10 11:16:26 -0500

    Added function to see users only as an admin

--- server/api/users.js
+++ server/api/users.js
@@ -2,7 +2,15 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
-router.get('/', (req, res, next) => {
+function adminMiddleware(req, res, next) {
+  if (req.user.isAdmin) {
+    next()
+  } else {
+    next('Unauthorized access!')
+  }
+}
+
+router.get('/', adminMiddleware, (req, res, next) => {
   User.findAll({
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
